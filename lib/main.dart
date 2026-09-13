481a2f503233dd44444284f8a85e718ea123b93e
mohammadeisazahi <amin.eisazahi@gmail.com>
2026-09-13 19:23:02 +0330

    Update social media icons in _social widget

--- lib/main.dart
+++ lib/main.dart
@@ -436,9 +436,9 @@ Widget _catTile(Cat c) {
 
 Widget _social() {
   final items = [
-    ['تلگرام', Icons.telegram, 'https://t.me/itarbiatbadani'],
-    ['اینستاگرام', Icons.instagram, 'https://instagram.com/itarbiatbadani'],
-    ['بله', Icons.comment, 'https://ble.ir/itarbiatbadani'],
+    ['تلگرام', Icons.send, 'https://t.me/itarbiatbadani'],
+    ['اینستاگرام', Icons.camera_alt, 'https://instagram.com/itarbiatbadani'],
+    ['بله', Icons.chat_bubble_outline, 'https://ble.ir/itarbiatbadani'],
     ['فروشگاه', Icons.shopping_cart, '$site/shop/'],
   ];
   return Column(children: [
